fbf657ef1c11647ee0fabdda473eb6a716de81a6
DanPaul12 <dan.paul.schechter@gmail.com>
2024-08-19 00:13:45 -0700

    hw23

--- add_members.py
+++ add_members.py
@@ -13,10 +13,34 @@ def add_member(id, name, age):
             cursor.execute(query, values)
             conn.commit()
             print("New member added")
-        except Error as e:
-            print("Error: {e}")
-        
+        except Error:
+            print({Error})
 
+def add_workout_session(member_id, session_id, date, time, activity):
+    conn = connection()
+    if conn is not None:  
+        try:
+            cursor = conn.cursor()
+            query = "INSERT INTO WorkoutSessions Values (%s, %s, %s, %s, %s)"
+            values = member_id, session_id, date, time, activity
+            cursor.execute(query, values)
+            conn.commit()
+            print("New workout session added")
+        except Error:
+            print({Error})
+
+def update_member_age(member_id, new_age):
+    conn = connection()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            query = "UPDATE Members SET age = %s WHERE id = %s"
+            values = new_age, member_id
+            cursor.execute(query, values)
+            conn.commit()
+            print("Member age updated")
+        except Error:
+            print({Error})
 
 
 def delete_workout_session(session_id):
@@ -31,4 +55,5 @@ def delete_workout_session(session_id):
         except Error:
             print({Error})
 
-delete_workout_session(2, )
+
+update_member_age(1,38)
